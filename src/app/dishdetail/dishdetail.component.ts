e4dc10cc1596ee04a8d77e3ea83e58cde2842e29
extensaman <you@example.comverus.wedding@gmail.com>
2022-02-01 16:59:33 +0300

    16:59

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -8,7 +8,8 @@ import { Dish } from '../shared/dish';
 })
 export class DishdetailComponent implements OnInit {
 
-  @Input() dish: Dish | undefined;
+  @Input() 
+  dish: Dish | undefined;
 
   constructor() { }
 
