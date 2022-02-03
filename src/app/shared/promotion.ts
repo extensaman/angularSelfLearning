a7c84966a53d728041e4c743c96a316923783f39
extensaman <verus.wedding@gmail.com>
2022-02-03 21:51:47 +0300

    21_51

---
+++ src/app/shared/promotion.ts
@@ -0,0 +1,10 @@
+export class Promotion {
+    id: string | undefined;
+    name: string | undefined;
+    image: string | undefined;
+    label: string | undefined;
+    price: string | undefined;
+    featured: boolean | undefined;
+    description: string | undefined;
+
+}
