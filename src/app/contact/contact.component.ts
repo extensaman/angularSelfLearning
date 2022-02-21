9d3d85293230e6ab8539fa95cc74921a998c8093
extensaman <you@example.comverus.wedding@gmail.com>
2022-02-21 17:02:04 +0300

    17_01

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -14,16 +14,60 @@ export class ContactComponent implements OnInit {
   contactType = ContactType;
   @ViewChild('fform') feedbackFormDirective: FormGroupDirective | undefined;
 
+  formError = {
+    'firstname': '',
+    'lastname': '',
+    'telnum': '',
+    'email': ''
+  };
+
+  validationMessages = {
+    'firstname': {
+      'required': "First name is required",
+      'minLength': "First name must be at least 2 characters long",
+      'maxLength': "First name can't be more than 25 characters long"
+    },
+    'lastname': {
+      'required': "Last name is required",
+      'minLength': "Last name must be at least 2 characters long",
+      'maxLength': "Last name can't be more than 25 characters long"
+    },
+      'telnum': {
+        'required': "Tel.number is required",
+        'pattern': "Tel.number must contain only numbers"
+    },
+      'email': {
+        'required': "Email is required",
+        'email': "Email is not in valid format"
+      }
+  };
+
   constructor(private fb: FormBuilder) { 
     this.feedbackForm = this.fb.group({
-      firstname: ['', Validators.required],
-      lastname: ['', Validators.required],
-      telnum: [0, Validators.required],
-      email: ['', Validators.required],
+      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      telnum: [0, [Validators.required, Validators.pattern]],
+      email: ['', [Validators.required, Validators.email]],
       agree: false,
       contacttype: 'None',
       message: ''
     });
+
+    this.feedbackForm.valueChanges.subscribe(data => this.onValueChanged(data));
+    this.onValueChanged(); // (re)set form validation messages
+  }
+
+  onValueChanged(data?: any) {
+    if(!this.feedbackForm) {
+      return;
+    }
+
+    const form = this.feedbackForm;
+    for (const field in this.formError) {
+      if(this.formError.hasOwnProperty(field)){
+        
+      }
+    }
   }
 
   ngOnInit(): void {
